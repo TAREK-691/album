af82d84c1e8465792542fa752fe8e91c75b10218
itachi-prime99 <ovhi.bbz108@outlook.com>
2025-04-14 09:05:14 +0600

    Delete Video.js

--- Video.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const videoSchema = new mongoose.Schema({
-  category: String,
-  url: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Video", videoSchema);
